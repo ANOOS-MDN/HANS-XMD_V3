8f3d874772955ae73c267b339b0e773d890b2727
الامبراطور انوس <vhhgvgghhvh1@gmail.com>
2025-07-27 01:03:09 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "HansTz&PtolUR6B#RjejEaWVIpMUsXLEDguBpr2zKF27352YJUtrwf7vyLw",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
